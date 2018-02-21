f03284cba7e2f410b192ab1097d37f1f430869b1
Trevor Reeves <trevorreeves@gmail.com>
2018-02-21 21:41:42 +0000

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jule
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/jule/core.clj
@@ -0,0 +1,7 @@
+(ns jule.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
